5ec0c6b42959b1a429f0cd3a6020739111d6aef5
Andre Macedo <andrefrmacedo@gmail.com>
2016-04-22 17:35:06 +0100

    Updated RDS endpoint

---
+++ config.py
@@ -0,0 +1,9 @@
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://car:dealership@localhost/car_dealership'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://car:dealership@cardealership.cxnnhvbijgdi.us-west-2.rds.amazonaws.com:3306/car_dealership'
+S3_LOCATION = 'https://s3-eu-west-1.amazonaws.com'
+S3_KEY = 'AKIAIEM2MF6Z25LVMIEQ'
+S3_SECRET = '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'
+S3_UPLOAD_DIRECTORY = '/img'
+S3_BUCKET = 'cardealershipasaservice'
+
+SECRET_KEY = "FLASK_SECRET_KEY"
